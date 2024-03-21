ec67d63e160e3ed9c0f9200b6c4ffce648eb5799
Avery Krum <averykru@in-f1.nsk.21-school.ru>
2024-03-21 12:16:58 +0700

    func

--- ex9/main.c
+++ ex9/main.c
@@ -1,15 +1,67 @@
 #include "main.h"
 #include <dlfcn.h>
+#include <string.h>
+
+#define NAME_SIZE 50
+
 void menu();
 void number_choose(int *a, int *b);
 void lib_choose();
 
+int *inicialization(char *name){
+    char dir[] = "./libs/";
+    char name_with_dir[NAME_SIZE + strlen(dir)];
+    char function_name[NAME_SIZE];
+    name[strlen(name) - 1] = '\0';
+
+    for(int i = 0; i < NAME_SIZE; i++){
+        name_with_dir[i] = name[i];     
+    } 
+
+
+    memmove(name_with_dir + strlen(dir), name_with_dir, strlen(name_with_dir) + 1);
+    memcpy(name_with_dir, dir, strlen(dir));
+
+    void *ptr_lib = dlopen(name_with_dir, RTLD_LAZY);
+    if(!ptr_lib){
+        printf("%s\n",dlerror());
+        return 0;
+    }
+    int j = 2;
+    for(int i = 0; i < strlen(name); i++){
+        j++;
+        if(i < 4){
+            function_name[i] = name[j];    
+        }else if (i > strlen(name) - 4){
+            function_name[i] = '\0';
+        }else{
+            function_name[i] = name[i];    
+        }
+
+    } 
+
+    //printf("Введите имя \n");
+    
+    printf("func - %s\n", function_name);
+
+    //int (*ptr_func)() = dlsym(ptr_lib, "summary");
+    
+
+}
+
 int main(){
     int f = 1, lib_f = 1, num_a, num_b, choose, result, lib_count = 0;
-    void *ptr_sum = dlopen("./libs/libsummary.so",  RTLD_LAZY);
+    char name[NAME_SIZE];
+    printf("Введите имя бибилотеки в формате libname.so\n");
+
+    fgets(name, NAME_SIZE, stdin);
+    printf("name -  %s\n", name);
+    inicialization(name);
+    void *ptr_sum = dlopen("./libs/libsummary.so" ,  RTLD_LAZY);
     void *ptr_dif = dlopen("./libs/libdifference.so",  RTLD_LAZY);
     void *ptr_mult = dlopen("./libs/libdivision.so",  RTLD_LAZY);
     void *ptr_div = dlopen("./libs/libmultiplication.so",  RTLD_LAZY);
+    
     if(!ptr_sum){
         printf("%s\n",dlerror());
         return 0;
