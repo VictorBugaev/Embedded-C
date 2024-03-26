fc22925bddb4891ba494eba0c27865bdf9d1af1d
Victor <forpsbugaev@yandex.ru>
2024-03-26 12:12:36 +0700

    ex10_26.03_12:12

--- ex10/main.c
+++ ex10/main.c
@@ -2,25 +2,36 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 int main(){
-    int fd = open("output.txt", O_CREAT || O_RDWR);  
+    int fd = open("output.txt", O_CREAT | O_RDWR, 0777);  
     if(fd == -1){
         perror("open");
     }
     char *buf = "String from file";   
-    char *get_buf;
+    char get_buf[256];
     int str_size = strlen(buf);
-    write(fd, buf, str_size);   
-    if(lseek(fd, 0, str_size)==-1){
+    if(write(fd, buf, str_size)==-1){
+        perror("write");
+        exit(0);
+
+    }
+    if(lseek(fd, 0, SEEK_SET)==-1){
         perror("lseek");
+        exit(0);
+
     }
-    int pos = 1;
-    while(str_size - pos){
-        if(lseek(fd, str_size - pos, 1)==-1){
+    
+    for(int i = 0; (str_size + 1) - i > 0; i++){
+        if(lseek(fd, str_size - i, SEEK_SET)==-1){
             perror("lseek");
+            exit(0);
+
         }
-        read(fd, get_buf, 1);
-        printf("%s\n", get_buf);
-        pos++;
+        if(read(fd, get_buf, strlen(buf)) == -1){
+            perror("read");
+            exit(0);
+        }
     }
+    printf("%s\n", get_buf);    
 }
