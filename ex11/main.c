9a5fea30ceea9f2b5b783089118c7faf5b8f2494
Avery Krum <averykru@in-f4.nsk.21-school.ru>
2024-04-03 20:47:08 +0700

    03.04_20:46_file_manager

--- ex11/main.c
+++ ex11/main.c
@@ -1,189 +1,124 @@
-#include <stdio.h>
-#include <ncurses.h>
+#include "main.h"
 
-#include <sys/types.h>
 #include <dirent.h>
-
-
-#include <sys/stat.h>
-#include <time.h>
-
+#include <ncurses.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+int main() {
+  int rows, cols;
+  int key;
+  int current_position = 2;
+  int wind_num = 0;
+  int content = 0;
+  time_t lastClickTime = 0;
+  int k = 1;
+  int f = 0;
+  char dir_name[1000000] = "./";
+  char wind1_dir_name[1000000] = "./";
+  char wind2_dir_name[1000000] = "./";
+  char current_name[10000] = "./";
 
-#define DOUBLE_CLICK 2
+  WINDOW *main;
+  WINDOW *wnd[2];
 
-void transition(){
+  initscr();
+  cbreak();
+  curs_set(0);
+  keypad(stdscr, TRUE);
+  noecho();
+  start_color();
+  init_pair(1, COLOR_WHITE, COLOR_BLUE);
+  refresh();
+  getmaxyx(stdscr, rows, cols);
+  main = newwin(rows, cols, 0, 0);
+  wbkgd(main, COLOR_PAIR(1));
+  for (int i = 0; i < 2; i++) {
+    wnd[i] = derwin(main, rows, cols / 2, 0, i * cols / 2);
+    box(wnd[i], '|', '-');
+    wrefresh(wnd[i]);
+  }
+  mousemask(BUTTON1_CLICKED, NULL);
 
-}
+  print_wind(main, wnd, 0, current_position, dir_name, &content, current_name,
+             f);
+  print_wind(main, wnd, 1, current_position, dir_name, &content, current_name,
+             f);
 
-
-void set_color(WINDOW *name, WINDOW *size, WINDOW *time, int num){
-    wbkgd(name, COLOR_PAIR(num));
-    wbkgd(time, COLOR_PAIR(num)); 
-    wbkgd(size, COLOR_PAIR(num));         
-}
-
-int print_wind(WINDOW *main, WINDOW *wnd[], int wind_num, int current_position, char *dir_name, int * count, char *current_name, int f){
-    int rows, cols;
-    getmaxyx(stdscr, rows, cols);
-    DIR * dir = opendir(dir_name);
-    if(!dir){
-        return 0;
-    }
-    struct dirent * str;
-    struct stat file;
-    for(int i = 0; i < 2; i++){
-        wnd[i] = derwin(main, rows, cols/2, 0, i*cols/2);
-        box(wnd[i], '|', '-');
-        wrefresh(wnd[i]);
-    }
-    WINDOW *string[rows];
-    WINDOW *name[rows];
-    WINDOW *size[rows];
-    WINDOW *time[rows];
-    WINDOW *wind_box;
-    init_pair(2, COLOR_YELLOW, COLOR_BLUE);
-    init_pair(3, COLOR_BLUE, COLOR_WHITE);
-    wind_box = derwin(wnd[wind_num], rows, cols/11, 0, cols/6);
-    for(int i = 1; i < rows-1; i++){
-        string[i] = derwin(wnd[wind_num], 1, cols/2, i, 0);
-        name[i] = derwin(string[i], 1, cols/6, 0, 1);
-        size[i] = derwin(string[i], 1, cols/11, 0, cols/6);
-        time[i] = derwin(string[i], 1, cols/4-1, 0, cols/4);
-        wrefresh(string[i]);
-        wclear(name[i]);
-        wclear(size[i]);
-        wclear(time[i]);
-    }
-    
-    for (int i = 1; ((str = readdir(dir)) != NULL); i++){ 
-        if (i == 1){
-            set_color(name[i], size[i], time[i], 2);
-            wprintw(name[i], " Name");
-            wprintw(size[i], " Size");
-            wprintw(time[i], "   Time");
-            i++;
+  while ((key = getch()) != 'q') {
+    switch (key) {
+      case KEY_UP:
+        if (current_position > 2) {
+          current_position--;
+          print_wind(main, wnd, wind_num, current_position, dir_name, &content,
+                     current_name, f);
         }
-       if(i == current_position){
-            set_color(name[i], size[i], time[i], 3);
+        break;
+      case KEY_DOWN:
+        if (current_position < content) {
+          current_position++;
+          print_wind(main, wnd, wind_num, current_position, dir_name, &content,
+                     current_name, f);
         }
-        if(i == current_position && f){
-            strcat(current_name, str -> d_name);
-            strcat(current_name, "/");
+        break;
+      case '\n':
+        f = 1;
+        int ch = 0;
+        print_wind(main, wnd, wind_num, current_position, current_name,
+                   &content, current_name, f);
+        if (dir_check(current_name)) {
+          strcpy(dir_name, current_name);
+        } else {
+          strcpy(current_name, dir_name);
         }
-        wprintw(name[i], " %s \n", str -> d_name);
-        wprintw(size[i], " %d \n", str -> d_reclen);
-        if(stat(dir_name, &file) < 0){
-            break;
+
+        f = 0;
+        print_wind(main, wnd, wind_num, current_position, dir_name, &content,
+                   current_name, f);
+        break;
+      case 9:
+        wind_num = k % 2;
+        current_position = 2;
+        if (wind_num == 1) {
+          strcpy(wind1_dir_name, current_name);
+          memset(current_name, 0, strlen(current_name));
+          strcpy(current_name, wind2_dir_name);
+        } else {
+          strcpy(wind2_dir_name, current_name);
+          memset(current_name, 0, strlen(current_name));
+          strcpy(current_name, wind1_dir_name);
         }
-        wprintw(time[i], "   %s", ctime(&file.st_mtime));
-        *count = i;
-        wrefresh(name[i]);
-        wrefresh(size[i]);
-        wrefresh(time[i]);
-    }
-    box(wind_box, '|', '-');
-    wrefresh(main);
-    refresh();
-}
-
-int main(){
-    int rows, cols;
-    int key;
-    int current_position = 2;
-    int wind_num = 0;
-    int content = 0;
-    
-    time_t lastClickTime = 0;
-    
-    char current_name[10000]="./";
-    
-    WINDOW *main;    
-    WINDOW *wnd[2];
-
-    initscr();
-    cbreak();
-    curs_set(0);
-    keypad(stdscr, TRUE);
-    noecho();
-    start_color();
-    init_pair(1, COLOR_WHITE, COLOR_BLUE); 
-
-    refresh();
-    
-    getmaxyx(stdscr, rows, cols);
-    main =  newwin(rows, cols, 0, 0);
-    wbkgd(main, COLOR_PAIR(1)); 
-
-    for(int i = 0; i < 2; i++){
-        wnd[i] = derwin(main, rows, cols/2, 0, i*cols/2);
-        box(wnd[i], '|', '-');
-        wrefresh(wnd[i]);
-    }
-    //refresh();
-    mousemask(BUTTON1_CLICKED, NULL);
-    mousemask(BUTTON1_CLICKED, NULL);
-    int k = 1;
-    int f = 0;
-
-    char dir_name[100000] = "./"; 
-    print_wind(main, wnd, wind_num, current_position, dir_name, &content, current_name, f);
-    print_wind(main, wnd, k, current_position, dir_name, &content, current_name, f);
-    
-    wrefresh(main);
-    refresh();
-    while ((key = getch()) != 'q') {
-        switch (key) {
-        case KEY_UP:
-            if(current_position > 2) current_position --;
-            print_wind(main, wnd, wind_num, current_position, dir_name, &content, current_name, f);
-            break;
-        case KEY_DOWN:
-            if(current_position < content) current_position ++;
-            print_wind(main, wnd, wind_num, current_position, dir_name, &content, current_name, f);
-            break;
-        case '\n':
+        strcpy(dir_name, current_name);
+        print_wind(main, wnd, wind_num, current_position, dir_name, &content,
+                   current_name, f);
+        k++;
+        break;
+      case KEY_MOUSE:
+        MEVENT event;
+        if (getmouse(&event) == OK) {
+          time_t currentTime = time(NULL);
+          if (lastClickTime != 0 &&
+              difftime(currentTime, lastClickTime) < DOUBLE_CLICK) {
             f = 1;
-            print_wind(main, wnd, wind_num, current_position, current_name, &content, current_name, f);
-            strcpy(dir_name, current_name);
+            print_wind(main, wnd, wind_num, current_position, current_name,
+                       &content, current_name, f);
+            strcat(dir_name, current_name);
             f = 0;
-            break;
-        case 9:
-            wind_num = k % 2;
-            current_position = 2;
-            print_wind(main, wnd, wind_num, current_position, dir_name, &content, current_name, f);
-            k++;
-            break;
-        case KEY_MOUSE:
-            MEVENT event;
-            if (getmouse(&event) == OK) {
-                time_t currentTime = time(NULL);
-                if (lastClickTime != 0 && difftime(currentTime, lastClickTime) < 3) {
-                    //wprintw(main, "Double click detected \n");
-                    //wprintw(main, "cur - %d last - %d\n", currentTime, lastClickTime);
-                    f = 1;
-                    print_wind(main, wnd, wind_num, current_position, current_name, &content, current_name, f);
-                    strcpy(dir_name, current_name);
-                    f = 0;
-                }
-                lastClickTime = currentTime;
-            }
-            break;
-        /*case BUTTON1_DOUBLE_CLICKED:
-            f = 1;
-            print_wind(main, wnd, wind_num, current_position, current_name, &content, current_name, f);
-            strcpy(dir_name, current_name);
-            f = 0;
-            break;*/
-        default:
-            break;
+          } else {
+            print_wind(main, wnd, wind_num, event.y, current_name, &content,
+                       current_name, f);
+          }
+          lastClickTime = currentTime;
         }
-        refresh();  
+        break;
+      default:
+        break;
     }
-
     refresh();
-
-    //getch();
-    endwin();
+  }
+  refresh();
+  endwin();
 }
---
+++ ex11/main.h
@@ -0,0 +1,15 @@
+#include <dirent.h>
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+
+#define DOUBLE_CLICK 3
+
+void set_color(WINDOW *name, WINDOW *size, WINDOW *time, int num);
+int print_wind(WINDOW *main, WINDOW *wnd[], int wind_num, int current_position,
+               char *dir_name, int *count, char *current_name, int f);
+int dir_check(char *name);
