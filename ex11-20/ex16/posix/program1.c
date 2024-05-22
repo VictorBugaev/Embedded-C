d8ecade9b8fa32440aaa07a905f1acc3e03d5b11
Avery Krum <averykru@in-f1.nsk.21-school.ru>
2024-05-22 21:25:13 +0700

    edwd

--- ex11-20/ex16/posix/program1.c
+++ ex11-20/ex16/posix/program1.c
@@ -1,57 +1,57 @@
+#define _POSIX_C_SOURCE 200112L
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
 #include <sys/types.h>
-
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <semaphore.h>
+
 #define LEN 256
 
 int main() {
-  int sem_fd;
-  int fd;
+  sem_t * sem_fd;
+  int shm_fd;
   char *snd_message = "Message from program1";
   char rcv_message[256];
-  semctl(sem_fd, 0, SETVAL, 0);
-  struct sembuf sem_op;
-  sem_op.sem_num = 0;
-  sem_op.sem_op = -1;
-  sem_op.sem_flg = 0;
 
   char * sem = ".";
-  char * shm = "program1";
+  const char * shm = "/shm";
 
 
-  if ((sem_fd = sem_open(sem, O_CREAT, 0666, 1)) == -1) {
+  if ((sem_fd = sem_open(sem, O_RDWR, 0666, 1)) == (sem_t *) -1) {
     perror("semget");
     return -1;
   }
 
-  if ((shm_fd = shm_open(shm, O_CREAT | O_RDWR, 0666)) == -1) {
+  if ((shm_fd = shm_open(shm, O_CREAT | O_RDWR | O_TRUNC, 0666)) == -1) {
     perror("shm_open");
     return -1;
   }
 
-  if ((shm_ftruncate(shm_fd, LEN)) == -1) {
+  if ((ftruncate(shm_fd, LEN)) == -1) {
     perror("shm_open");
     return -1;
   }
 
-  char *ptr = mmap(NULL, LEN, PROT_WRITE|PROT_READ, MAP_SHARED, fd, 0);
+  char *ptr = mmap(NULL, LEN, PROT_WRITE|PROT_READ, MAP_SHARED, shm_fd, 0);
 
   if (ptr == (char *)-1) {
     perror("shmat");
     return -1;
   }
+  sem_wait(sem_fd); // Ждем, пока семафор не станет доступен
 
- // strncpy(ptr, snd_message, strlen(snd_message));
-//  semop(sem_fd, &sem_op, 1);
-//  printf("%s\n", ptr);
+  sem_wait(sem_fd); // Ждем, пока семафор не станет доступен
+  strncpy(ptr, snd_message, strlen(snd_message));
+  sem_post(sem_fd); // Освобождаем семафор
 
-  if (shmdt(ptr) == -1) {
+  printf("Message sent: %s\n", ptr);
+  if (munmap(ptr, LEN) == -1) {
     perror("shmdt");
     return -1;
   }
---
+++ ex11-20/ex16/posix/program2.c
@@ -0,0 +1,61 @@
+#define _POSIX_C_SOURCE 200112L
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+#define LEN 256
+
+int main() {
+  sem_t * sem_fd;
+  int shm_fd;
+  char *snd_message = "Message from program1";
+  char rcv_message[256];
+
+  char * sem = ".";
+  char * shm = "program1";
+
+
+  if ((sem_fd = sem_open(sem, 0666, 1)) == (sem_t *) -1) {
+    perror("semget");
+    return -1;
+  }
+
+  if ((shm_fd = shm_open(shm, O_RDWR, 0666)) == -1) {
+    perror("shm_open");
+    return -1;
+  }
+
+  if ((ftruncate(shm_fd, LEN)) == -1) {
+    perror("shm_open");
+    return -1;
+  }
+
+  char *ptr = mmap(NULL, LEN, PROT_WRITE|PROT_READ, MAP_SHARED, shm_fd, 0);
+
+  if (ptr == (char *)-1) {
+    perror("shmat");
+    return -1;
+  }
+
+  sem_wait(sem_fd); // Ждем, пока семафор не станет доступен
+  strncpy(ptr, snd_message, strlen(snd_message));
+  sem_post(sem_fd); // Освобождаем семафор
+
+  printf("Message sent: %s\n", ptr);
+  if (shmdt(ptr) == -1) {
+    perror("shmdt");
+    return -1;
+  }
+  if (shm_unlink(shm) == -1) {
+    perror("shmctl");
+    return -1;
+  }
+}
