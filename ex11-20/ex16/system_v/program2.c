e4c29d068b21f4dc4bc4d26a272bcabd2dcebebf
Avery Krum <averykru@in-f1.nsk.21-school.ru>
2024-05-22 19:59:43 +0700

    22.02

--- ex11-20/ex16/system_v/program2.c
+++ ex11-20/ex16/system_v/program2.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include <string.h>
-#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/types.h>
 
+int main() {
+  int sem_fd;
+  int fd;
+  char *snd_message = "Message from program2";
+  char rcv_message[256];
+  key_t sem_key = ftok(".", 1);
 
-int main(){
-    key_t key = ftok("shared_memory", 1);
-    int fd;
-    char *snd_message = "Message from program2";
-    char rcv_message[256];
-    if(key == -1){
-        perror("ftok");
-        return -1;
-    }
-    if((fd = shmget(key, 100, 0)) == -1){
-        perror("shmget");
-        return -1;
-    }
-    char *ptr = (char *)shmat(fd, NULL, 0);
-    if(ptr == (char *)-1){
-        perror("shmat");
-        return -1;
-    }   
-    //while(1){
-        //strncpy(rcv_message, ptr, 100);
-        printf("%s", ptr);
-        //sleep(10);
-        strncpy(ptr, snd_message, strlen(snd_message));
-    //}
+  if (sem_key == -1) {
+    perror("sem ftok");
+    return -1;
+  }
+  key_t key = ftok("program1", 1);
+  if (key == -1) {
+    perror("ftok");
+    return -1;
+  }
+  if ((sem_fd = semget(key, 1, 0666)) == -1) {
+    perror("semget");
+    return -1;
+  }
+  if ((fd = shmget(key, 100, 0)) == -1) {
+    perror("shmget");
+    return -1;
+  }
+  char *ptr = (char *)shmat(fd, NULL, 0);
+  if (ptr == (char *)-1) {
+    perror("shmat");
+    return -1;
+  }
 
-    if(shmdt(ptr) == -1){
-        perror ("shmdt");
-        return -1;
-    }
-    if(shmctl(fd, IPC_RMID, 0) == -1){
-        perror ("shmctl");
-        return -1;    
-    }
+  printf("%s\n", ptr);
+  strncpy(ptr, snd_message, strlen(snd_message));
+  semctl(sem_fd, 0, SETVAL, 1);
+
+  if (shmdt(ptr) == -1) {
+    perror("shmdt");
+    return -1;
+  }
 }
