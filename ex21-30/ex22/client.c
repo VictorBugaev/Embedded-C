a51eeefe0a3994b2f8b108d6bfae4bf490b54c87
Victor <forpsbugaev@yandex.ru>
2024-06-16 17:24:03 +0700

    16.06

---
+++ ex21-30/ex22/client.c
@@ -0,0 +1,57 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define DEST_PORT 20000
+#define SRC_PORT 20001
+#define PAYLOAD_SIZE 248
+int main() {
+  int fd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+
+  if (fd == -1) {
+    perror("socket");
+    return -1;
+  }
+
+  struct udphdr udp;
+
+  udp.dest = htons(DEST_PORT);
+  udp.source = htons(SRC_PORT);
+  udp.check = 0;
+  udp.len = htons(PAYLOAD_SIZE + sizeof(udp));
+
+  char message[sizeof(udp) + PAYLOAD_SIZE];
+  memcpy(message, &udp, sizeof(udp));
+  char payload[PAYLOAD_SIZE] = "Hello from client!\n";
+  memcpy(message + sizeof(udp), payload, strlen(payload));
+
+  struct sockaddr_in addr;
+  int size = sizeof(struct sockaddr_in);
+
+  if (sendto(fd, message, sizeof(message), 0, (struct sockaddr *)&addr, size) ==
+      -1) {
+    perror("sendto");
+    return -1;
+  }
+
+  while (1) {
+    if (recvfrom(fd, message, PAYLOAD_SIZE, 0, (struct sockaddr *)&addr,
+                 &size) == -1) {
+      perror("recvto");
+      return -1;
+    }
+
+    printf("%s\n", message);
+  }
+
+  if ((close(fd)) == -1) {
+    perror("close");
+    return -1;
+  }
+}
